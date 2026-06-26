88a47348459b3f6806f4cb059d50190caf61cd90
NuhaminAtomsa <nuhaminatomsa17@gmail.com>
2026-06-26 06:41:13 +0300

    message

---
+++ Problem Solving/SockMerchant.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+                                  
+def sockMerchant(n, ar):
+    freq = {}
+
+    for sock in ar:
+        if sock in freq:
+            freq[sock] += 1
+        else:
+            freq[sock] = 1
+
+    pairs = 0
+
+    for count in freq.values():
+        pairs += count // 2
+
+    return pairs
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
